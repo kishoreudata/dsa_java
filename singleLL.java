259eaec61175a78444277c31b974beaf946f61b2
kishoreudata <36151863+kishoreudata@users.noreply.github.com>
2021-09-15 23:31:32 +0530

    Add files via upload

---
+++ singleLL.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class singleLL {
+	public static void main(String args[]) {
+			LinkedList<Integer>ll=new LinkedList<Integer>();
+			//LinkedList<String> ll= new LinkedList<String>();
+			ll.add(1);
+			ll.add(2);
+			ll.add(3);
+			System.out.println(ll);
+	}
+}
