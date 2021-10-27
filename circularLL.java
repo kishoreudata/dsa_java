d559f286cacbac951d8bd6ed3195cef972387c04
kishoreudata <36151863+kishoreudata@users.noreply.github.com>
2021-10-27 19:02:13 +0530

    Add files via upload

--- circularLL.java
+++ circularLL.java
@@ -8,6 +8,11 @@ public class circularLL {
         list1.insertinBeginning(1);
         //input n=5 2 3 5 6 7
         list1.display();
+        list1.deleteFirstNode();
+        list1.deleteLastNode();
+        list1.deleteNode(3);
+        System.out.println("After deletion of some nodes ");
+        list1.display();
     }
     class Node{
         public int data;
@@ -76,6 +81,51 @@ public class circularLL {
             p.next=temp;
             if(p==last)last=temp;
         }
+    }
+    public void deleteFirstNode(){
+        if(last==null)return;
+        if(last.next==last){
+            last=null;
+            return;
+        }
+        last.next=last.next.next;
+    }
+    public void deleteLastNode(){
+        if(last==null)return;
+        if(last.next==last){
+            last=null;
+            return;
+        }
+        Node p=last.next;
+        while (p.next!=last){
+            p=p.next;
+        }
+        p.next=p.next.next;
+        //or p.next = last.next;
+        last=p;
+    }
+    public void deleteNode(int x){
+        if(last==null)return;
+        if(last.next==last){
+            if(last.data==x) last=null;
+            else System.out.println(x+" is not present in the list");
+            return;
+        }
+        if(last.next.data==x){
+            last.next=last.next.next;
+            return;
+        }
+        Node p = last.next;
+        while (p.next!=last.next){
+            if(p.next.data==x)break;
+            p=p.next;
+        }
+        if(p.next==last.next)System.out.println(x+" is not present in the list");
+        else {
+            p.next=p.next.next;
+            if(last.data==x)last=p;
+        }
+
     }
 }
 
